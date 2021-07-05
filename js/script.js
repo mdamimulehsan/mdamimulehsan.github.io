607e6dbb4ad9401a59f88f1a235443eaa5140c94
MD AMIMUL EHSAN <78925143+mdamimulehsan@users.noreply.github.com>
2021-07-05 23:04:10 +0600

    my portfolio completed

---
+++ js/script.js
@@ -0,0 +1,21 @@
+//typing animation scroll
+var typed = new Typed(".typing",{
+    strings: ["Programmer", "Frontend Designer", "Problem Solver", "Web Developer", "Programme Analyst", "Blogger", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+
+});
+
+var typed = new Typed(".typing-2",{
+    strings: ["Programmer", "Frontend Designer", "Problem Solver", "Web Developer", "Programme Analyst", "Blogger", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+
+});
+
+$(window).load(function() {
+    $('.preloader').fadeOut('slow');
+ });
+
